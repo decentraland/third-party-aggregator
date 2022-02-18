a66db34123a08dcca836e3c3b80e391da237b64e
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-02-18 13:20:29 -0300

    fix: history version

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -2,30 +2,26 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import { locations } from "../../modules/locations";
 import { Props } from "./Routes.types";
 
-const Routes = ({ isConnected }: Props) => {
-  if (!isConnected) {
-    return (
-      <>
-        <Switch>
+const Routes = ({ isConnected }: Props) => (
+  <>
+    <Switch>
+      {!isConnected ? (
+        <>
           <Route
             exact
             path={locations.signIn()}
-            render={() => <div>Foo</div>}
+            component={() => <div>Foo</div>}
           />
           <Redirect to={locations.signIn()} />
-        </Switch>
-      </>
-    );
-  }
-
-  return (
-    <>
-      <Switch>
-        <Route exact path={locations.root()} render={() => <div>Bar</div>} />
-        <Redirect to={locations.root()} />
-      </Switch>
-    </>
-  );
-};
+        </>
+      ) : (
+        <>
+          <Route exact path={locations.root()} component={() => <div>Bar</div>} />
+          <Redirect to={locations.root()} />
+        </>
+      )}
+    </Switch>
+  </>
+);
 
 export default Routes;
