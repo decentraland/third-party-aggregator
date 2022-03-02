7d335b6ee55e40a95e035b0b4fbd4859fc882c2f
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2022-03-02 12:19:01 -0500

    chore: Update variable name

--- .ci/index.ts
+++ .ci/index.ts
@@ -2,14 +2,14 @@ import { env, envTLD } from 'dcl-ops-lib/domain'
 import { buildStatic } from 'dcl-ops-lib/buildStatic'
 
 async function main() {
-  const account = buildStatic({
+  const thirdPartyAggregator = buildStatic({
     domain: `third-party-aggregator.decentraland.${env === 'prd' ? 'org' : envTLD}`,
     defaultPath: 'index.html',
   })
 
   return {
-    cloudfrontDistribution: account.cloudfrontDistribution,
-    bucketName: account.contentBucket,
+    cloudfrontDistribution: thirdPartyAggregator.cloudfrontDistribution,
+    bucketName: thirdPartyAggregator.contentBucket,
   }
 }
 export = main
